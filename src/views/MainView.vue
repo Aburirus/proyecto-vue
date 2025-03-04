bb4b98a9661fe19abedfd33a5a86cf02fb7db4a0
Abril <abrilcuinr@gmail.com>
2025-03-04 17:33:42 -0600

    add_footer

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,10 +5,15 @@
             <RouterView/>
         </div>
     </div>
+    <div>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script setup>
+import Footer from '../components/Footer.vue';
 import sidebar from '../components/SideBar.vue';
+
 </script>
 
 <style scoped>
