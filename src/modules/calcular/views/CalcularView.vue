f5d7ab9568758dbb5f55a4e9310868f93db3e9ce
Abril <abrilcuinr@gmail.com>
2025-03-14 19:11:03 -0600

    directivas

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -3,7 +3,13 @@
     <input type="text" v-model="valor">
     {{ valorCalculado }}
    
-    <ResultadoSection :resultado="valorCalculado"></ResultadoSection>
+    <ResultadoSection v-if="mostrarElemto" :resultado="valorCalculado"></ResultadoSection> 
+        <!-- el v-show y el v-if pueden servir para casi lo mismo -->
+         <div v-else-if="isNaN(valor)">De ser un numero para comprobar </div>
+    <div v-else> 
+        Aqui no se muestra nada
+    </div>
+    <!-- la directiva v-else se vincula directamente a la v-if que esta mas arriba pero nunca debe estar sola-->
 </template>
 
 <script setup>
@@ -14,6 +20,10 @@ const valor = ref('');
 const valorCalculado = computed(() => {
     return valor.value *3;
 })
+
+const mostrarElemto = computed(() => {
+    return valor.value > 10; 
+});
 
 </script>
 
